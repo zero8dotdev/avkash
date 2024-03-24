9fe7a222c3c0234dc4c1e0eacb00fa8bdd3a1e19
Ashutosh Tripathi <hello@zero8.dev>
2024-03-24 19:27:46 +0530

    resolved conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./_components/Header";
+import Header from "./_components/header";
 import Link from "next/link";
 import Image from "next/image";
 
