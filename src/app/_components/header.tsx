3eb678c1d3da11e56b4329724c29fda8864bfdca
Ashutosh Tripathi <hello@zero8.dev>
2024-03-24 13:54:39 +0530

    feat: basic routing setup

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="h-16 bg-white border-r-emerald-500 p-4">
+      <h1 className="text-red-500 text-lg">Avkash</h1>
+    </header>
+  );
+}
