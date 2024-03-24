b7819072d3e34271ea546a09b3c511a45ab19368
Ashutosh Tripathi <hello@zero8.dev>
2024-03-24 19:19:53 +0530

    feat: slack login flow test

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,16 @@
-export default function Header() {
+import Link from "next/link";
+
+export default function Header({
+  rightMenu,
+}: Readonly<{
+  rightMenu: React.ReactNode;
+}>) {
   return (
-    <header className="h-16 bg-white border-r-emerald-500 p-4 border-y">
-      <h1 className="text-red-500 text-lg">Avkash</h1>
+    <header className="h-16 flex justify-between items-center bg-white border-r-emerald-500 p-4 border-y">
+      <Link href="/">
+        <h1 className="text-red-500 text-lg">Avkash</h1>
+      </Link>
+      {rightMenu}
     </header>
   );
 }
