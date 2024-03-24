3cbec0425c8f63e3e21654805d38255e8d18ba6e
kesavaDk <kesavad1652@gmail.com>
2024-03-24 17:14:47 +0530

    created login component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
 export default function Login() {
-  return <h1 className="font-bold">Login</h1>
+  return(
+    <div className="flex  justify-center items-center min-h-screen" >
+      <div className="text-center space-x-3">
+      <h1 className="font-bold">Sign in to Avkash</h1>
+      <button className="w-72 rounded-md m-4 p-1 bg-pink-700" >Sign in with Slack</button><br/>
+      <button className="w-72 rounded-md p-1 bg-green-800">Sign in with Email</button>
+      </div>
+    </div>
+  )
 }
