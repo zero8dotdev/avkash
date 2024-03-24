1cb070962bdaad5f910b1125cfadd3807cd0a953
Ashutosh Tripathi <hello@zero8.dev>
2024-03-24 19:23:11 +0530

    fix: missing import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./_components/header";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main className="min-h-dvh bg-gray-100">
-        {children}
-        </main>
+        <main className="min-h-dvh bg-gray-100">{children}</main>
       </body>
     </html>
   );
