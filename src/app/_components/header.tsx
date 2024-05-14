147b25a8fe6c761d9735739cea3d66052b0df096
Ashutosh Tripathi <hello@zero8.dev>
2024-05-14 14:34:27 +0530

    few changes while review

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -7,7 +7,7 @@ export default async function Header() {
   const session = await supabase.auth.getSession();
 
   const rightMenu = !session ? (
-    <div>
+    <div className="w-[300px] flex flex-row-reverse gap-1">
       <Link href="/login">
         <button className="p-2 bg-sky-500 transition-colors hover:bg-sky-700 mr-2  text-white">
           Login
@@ -21,16 +21,26 @@ export default async function Header() {
       </Link>
     </div>
   ) : (
-    <div>
+    <div className="w-[300px] flex flex-row-reverse gap-1">
       <Image
         src="https://lh3.google.com/u/0/ogw/AF2bZyh0K8u-8ihV-8IYLKSnRPhHx6FwrEjtXEtd4XTM=s32-c-mo"
         height={36}
         width={36}
         alt="Ashutosh Tripathi"
       />
-      <Link href="/logout">
+      {/* <Link href="/logout">
         <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
           Logout
+        </button>
+      </Link> */}
+      <Link href="/signup">
+        <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
+          Signup
+        </button>
+      </Link>
+      <Link href="/login">
+        <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
+          Login
         </button>
       </Link>
     </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,10 +6,10 @@ import { createClient } from "@supabase/supabase-js";
 
 import { useRouter } from "next/navigation";
 
-const supabaseUrl = "https://cmdxjdjcazwuevappeku.supabase.co";
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtZHhqZGpjYXp3dWV2YXBwZWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxMTM1NzcsImV4cCI6MjAyODY4OTU3N30.8mRmHq3JPiyRoF58dFnf-M4QkaTj8T_5Vk23PUFeoeA";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+);
 
 export default function SignUp() {
   const router = useRouter();
@@ -27,15 +27,15 @@ export default function SignUp() {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    
 
+    // Step 1: Check if that org with same name is alreay created?
+    // Then creation step as it is
     try {
       const { data: orgData, error: orgError } = await supabase
         .from("Organisation")
         .insert([
           {
             name: formData.company,
-            
           },
         ])
         .select();
@@ -43,13 +43,9 @@ export default function SignUp() {
       if (orgError) {
         throw orgError;
       }
-      
-      localStorage.setItem("orgId",orgData[0].orgId)
 
-      
+      localStorage.setItem("orgId", orgData[0].orgId);
 
-
-    
       const { data: teamData, error: teamError } = await supabase
         .from("Team")
         .insert([
@@ -59,10 +55,9 @@ export default function SignUp() {
           },
         ])
         .select();
-       
+
       if (teamData) {
-        
-        localStorage.setItem("teamId",teamData[0].teamId)
+        localStorage.setItem("teamId", teamData[0].teamId);
         const { data: userData, error: userError } = await supabase
           .from("User")
           .insert([
@@ -76,19 +71,15 @@ export default function SignUp() {
             },
           ])
           .select();
-          if (userData){
-            localStorage.setItem("userId",userData[0].userId)
+        if (userData) {
+          localStorage.setItem("userId", userData[0].userId);
           const { data, error } = await supabase
-          
-          .from("Team")
-          .update({ manager: userData[0].userId})
-          .eq("teamId", userData[0].teamId)
-          .select();
-          }
+            .from("Team")
+            .update({ manager: userData[0].userId })
+            .eq("teamId", userData[0].teamId);
+        }
       }
-      
 
-     
       setFormData({
         name: "",
         company: "",
@@ -102,9 +93,8 @@ export default function SignUp() {
   };
 
   return (
-    <div className="min-h-screen  flex flex-col justify-center items-center bg-gray-100">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
       <h1 className="text-xl font-bold mb-4">Avkash</h1>
-
       <form className="flex flex-col" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
         <input
@@ -117,7 +107,6 @@ export default function SignUp() {
           value={formData.name}
           onChange={handleChange}
         />
-
         <label htmlFor="company-name">Company Name</label>
         <input
           required
@@ -158,69 +147,6 @@ export default function SignUp() {
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md w-full"
         />
       </form>
-
-      {/* <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h2 className="text-xl font-bold mb-4">Sign-Up for Avkash</h2>
-        <div className=" bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4">
-          <p className="font-bold">Caution:</p>
-          <p>Is your organization using slack? Caution content goes here</p>
-        </div>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="firstName" className="block mb-1">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              className="border border-gray-300 rounded-md p-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="lastName" className="block mb-1">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="border border-gray-300 rounded-md p-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="companyName" className="block mb-1">
-              Company Name
-            </label>
-            <input
-              type="text"
-              id="companyName"
-              name="companyName"
-              className="border border-gray-300 rounded-md p-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-1">
-              Work Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="border border-gray-300 rounded-md p-2 w-full"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md w-full"
-          >
-            Sign Up
-          </button>
-        </form>
-        <p>
-          Already an account <span className="text-blue-500">Sign-In</span>
-        </p>
-      </div> */}
     </div>
   );
 }
