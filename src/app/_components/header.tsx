f77a245cdf6650238e1a549737be1b0dfd30295f
Ashutosh Tripathi <hello@zero8.dev>
2024-05-27 14:14:06 +0530

    progress in login

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,57 +1,17 @@
+"use client";
+
 import Link from "next/link";
-import Image from "next/image";
-import { createClient } from "@/app/_utils/supabase/server";
+import { Layout } from "antd";
+const { Header: AntHeader } = Layout;
 
-export default async function Header() {
-  const supabase = createClient();
-  const session = await supabase.auth.getSession();
-
-  const rightMenu = !session ? (
-    <div className="w-[300px] flex flex-row-reverse gap-1">
-      <Link href="/login">
-        <button className="p-2 bg-sky-500 transition-colors hover:bg-sky-700 mr-2  text-white">
-          Login
-        </button>
-      </Link>
-      <Link
-        href="/signup"
-        className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500"
-      >
-        Signup for free
-      </Link>
-    </div>
-  ) : (
-    <div className="w-[300px] flex flex-row-reverse gap-1">
-      <Image
-        src="https://lh3.google.com/u/0/ogw/AF2bZyh0K8u-8ihV-8IYLKSnRPhHx6FwrEjtXEtd4XTM=s32-c-mo"
-        height={36}
-        width={36}
-        alt="Ashutosh Tripathi"
-      />
-      {/* <Link href="/logout">
-        <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
-          Logout
-        </button>
-      </Link> */}
-      <Link href="/signup">
-        <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
-          Signup
-        </button>
-      </Link>
-      <Link href="/login">
-        <button className="p-2 bg-white transition-colors hover:bg-sky-100 border-2 border-sky-500">
-          Login
-        </button>
-      </Link>
-    </div>
-  );
-
+export default function Header() {
   return (
-    <header className="h-16 flex justify-between items-center bg-white border-r-emerald-500 p-4 border-y">
+    <AntHeader
+      style={{ display: "flex", alignItems: "center", backgroundColor: "#fff" }}
+    >
       <Link href="/">
         <h1 className="text-red-500 text-lg">Avkash</h1>
       </Link>
-      {rightMenu}
-    </header>
+    </AntHeader>
   );
 }
