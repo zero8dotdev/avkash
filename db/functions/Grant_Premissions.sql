247db59930682438b27ac9779f5e796c161a7b0b
Ashutosh Tripathi <hello@zero8.dev>
2024-06-05 13:10:48 +0530

    chore: some changes around execution script, env files

--- db/functions/Grant_Premissions.sql
+++ db/functions/Grant_Premissions.sql
@@ -2,9 +2,13 @@
 GRANT USAGE ON SCHEMA public TO supabase_auth_admin;
 
 -- Grant insert, update, delete, and select on all tables in the public schema to supabase_auth_admin
-GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA public TO supabase_auth_admin;
+GRANT INSERT,
+UPDATE,
+DELETE,
+SELECT
+  ON ALL TABLES IN SCHEMA public TO supabase_auth_admin;
 
 -- Grant execute on all functions in the public schema to supabase_auth_admin
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO supabase_auth_admin;
 
-GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
+-- GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
