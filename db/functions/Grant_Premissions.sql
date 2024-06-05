396bb182813e574e4ed5c7e6e9c162fba6210f25
Ashutosh Tripathi <hello@zero8.dev>
2024-06-05 15:48:48 +0530

    blunder mistake

--- db/functions/Grant_Premissions.sql
+++ db/functions/Grant_Premissions.sql
@@ -11,12 +11,8 @@ SELECT
 -- Grant execute on all functions in the public schema to supabase_auth_admin
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO supabase_auth_admin;
 
-<<<<<<< HEAD
--- GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
+GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
 grant usage on schema "public" to anon;
 grant usage on schema "public" to authenticated;
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA "public" TO authenticated;
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA "public" TO anon;
-=======
-GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
->>>>>>> 14138e1 (restored schema file)
