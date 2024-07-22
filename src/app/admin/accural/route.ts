365a41efe443a67afd1a8835f863909318560aa4
Ashutosh Tripathi <hello@zero8.dev>
2024-07-22 15:23:28 +0530

    feat: added a endpoint to be called from next cron scheduler

---
+++ src/app/admin/accural/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { calculateAccural } from "./_functions/calculateAccural";
+
+type IFrequency = 'MONTHLY' | 'QUARTERLY' | 'HALF_YEARLY';
+type IAccuredOn = 'BEGINNING' | 'END';
+
+export async function POST(request: Request) {
+  const { frequency, accuredOn }: { frequency: IFrequency, accuredOn: IAccuredOn } = await request.json();
+
+  if (!frequency) {
+    return new NextResponse(JSON.stringify({ error: `frequency is required.` }), { status: 400 });
+  };
+
+  const jobExecutionResult = await calculateAccural(frequency, accuredOn);
+  return new NextResponse(JSON.stringify({ result: jobExecutionResult }), { status: jobExecutionResult ? 200 : 500 });
+};
