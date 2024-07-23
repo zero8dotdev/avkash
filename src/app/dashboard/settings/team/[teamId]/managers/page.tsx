865683a152e5d0a13310a837561a4e4f4bb3d607
kesavaDk <kesavad1652@gmail.com>
2024-07-23 11:58:35 +0530

    i added some settings functionalities

--- src/app/dashboard/settings/team/[teamId]/managers/page.tsx
+++ src/app/dashboard/settings/team/[teamId]/managers/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client"
 
+import { Card, List, Select } from 'antd'
+import React, { useState } from 'react'
+
+
+const users=['keshav','ashuthosh','yashwanth']
 const Managers = () => {
+  const [user,setUser]=useState<any[]>([]);
+
+const onChange=(value:any)=>{
+  setUser([...user,value])
+}
+
   return (
-    <div>
-      managers
-    </div>
+    <Card title="teamname-users">
+    <Select style={{width:'100%'}} onChange={onChange}>
+        {users.map((i,index)=>{
+          return <Select.Option key={index} value={i}>{i}</Select.Option>
+        })}
+      </Select>
+      {
+      user.length!==0 ?
+      (
+        <List
+        dataSource={user}
+        renderItem={(i) => <List.Item style={{paddingLeft:'10px'}}>{i}
+        <List.Item.Meta>Edit0</List.Item.Meta></List.Item>}
+      />
+      ):null
+      }
+
+     
+    </Card>
   )
+
 }
 
 export default Managers
--- src/app/dashboard/settings/team/[teamId]/settings/page.tsx
+++ src/app/dashboard/settings/team/[teamId]/settings/page.tsx
@@ -1,11 +1,117 @@
-import React from 'react'
+"use client";
+import { Button, Card, Checkbox, Form, Input, Select } from "antd";
+import moment from "moment-timezone";
+import { useEffect, useState } from "react";
 
-const Settings = () => {
+const TeamSettings = () => {
+  const [timezones, setTimezones] = useState<any[]>([]);
+
+  // we need to fetch team details from backend
+  const team = {
+    name: "team1",
+    id: "603ca341-bb32-48bb-aed8-9e99d2b49153",
+    startOfWorkWeek: "Monday",
+    workWeek: ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY'],
+    timeZone: "Asia/kolkata",
+  };
+
+  useEffect(() => {
+    const allTimezones = moment.tz.names();
+    setTimezones(allTimezones);
+  }, []);
+
+  const onFinish = async(values: any) => {
+    console.log(values)
+  };
+
+  console.log(timezones);
   return (
-    <div>
-      settings
-    </div>
-  )
-}
+    <Card
+      className="justify-center flex flex-col shadow-xl "
+      title="Name-Settings"
+    >
+      <Form
+        onFinish={onFinish}
+        initialValues={{
+          name: team.name,
+          startOfWorkWeek: team.startOfWorkWeek,
+          workWeek: team.workWeek,
+          timeZone:team.timeZone,
+        }}
+      >
+        <Form.Item name="name" label="Team Name" initialValue={team.name}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Start of work week" name="startOfWorkWeek">
+          <Select className="ml-4 w-56" placeholder="Select start of work week">
+            <Select.Option value="MONDAY">Monday</Select.Option>
+            <Select.Option value="TUESDAY">Tuesday</Select.Option>
+            <Select.Option value="WEDNESDAY">Wednesday</Select.Option>
+            <Select.Option value="THURSDAY">Thursday</Select.Option>
+            <Select.Option value="FRIDAY">Friday</Select.Option>
+            <Select.Option value="SATURDAY">Saturday</Select.Option>
+            <Select.Option value="SUNDAY">Sunday</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="Work week" name="workWeek">
+          <Checkbox.Group>
+            <Checkbox className="ml-16" value="MONDAY">
+              Monday
+            </Checkbox>
 
-export default Settings
+            <Checkbox className="ml-16" value="TUESDAY">
+              Tuesday
+            </Checkbox>
+
+            <Checkbox className="ml-16" value="WEDNESDAY">
+              Wednesday
+            </Checkbox>
+
+            <Checkbox className="ml-16" value="THURSDAY">
+              Thursday
+            </Checkbox>
+
+            <Checkbox className="ml-16" value="FRIDAY">
+              Friday
+            </Checkbox>
+
+            <Checkbox className="ml-16" value="SATURDAY">
+              Saturday
+            </Checkbox>
+
+            <Checkbox className="ml-16" value="SUNDAY">
+              Sunday
+            </Checkbox>
+          </Checkbox.Group>
+        </Form.Item>
+        <Form.Item label="Time Zone" name="timeZone">
+          <Select
+            style={{ width: "200px" }}
+            className="ml-16"
+            showSearch
+            placeholder="Search to Select"
+            optionFilterProp="children"
+            filterOption={(input, option) =>
+              ((option?.label ?? "") as string).includes(input)
+            }
+            filterSort={(optionA, optionB) =>
+              ((optionA?.label ?? "") as string)
+                .toLowerCase()
+                .localeCompare(((optionB?.label ?? "") as string).toLowerCase())
+            }
+            options={timezones.map((timezone: string) => ({
+              value: timezone,
+              label: timezone,
+            }))}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" type="primary" danger>
+            Save
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+export default TeamSettings;
