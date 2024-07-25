1deca12930fc9680f17d285f82686df61c699bb9
kesavaDk <kesavad1652@gmail.com>
2024-07-25 15:42:30 +0530

    i added leave requests

---
+++ src/app/(authenticated)/dashboard/users/_components/activity.tsx
@@ -0,0 +1,29 @@
+
+import { Avatar, Steps } from "antd"
+
+
+export  default async function  Activity({ user }: { user: any }){
+  
+    return (
+        <>
+       <Steps
+       direction="vertical"
+            items={[
+                {
+                  title: 'Login',
+                  status: 'finish',
+                  icon: <Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=1`}/>,
+                  
+                },
+                {
+                  title: 'Verification',
+                  status: 'finish',
+                  icon: <Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=1`}/>,
+                },
+                
+              ]}
+       />
+        </>
+    )
+
+}
