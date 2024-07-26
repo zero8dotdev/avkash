64f0fc63baeef5993ee76e5914c927315a8137f4
kesavaDk <kesavad1652@gmail.com>
2024-07-26 14:52:05 +0530

    added leave policies into org level and added custom add leave type

--- src/app/(authenticated)/dashboard/settings/leave-types/page.tsx
+++ src/app/(authenticated)/dashboard/settings/leave-types/page.tsx
@@ -9,12 +9,18 @@ import {
   Button,
   Popconfirm,
   PopconfirmProps,
+  Modal,
+  Form,
+  Input,
+  ColorPicker,
+  Checkbox,
+  Select,
 } from "antd";
 import { CheckCircleTwoTone, CloseCircleTwoTone } from "@ant-design/icons";
 import { useState, useEffect } from "react";
 import LeaveTypeEdit from "./leaveEditType";
 import { useApplicationContext } from "@/app/_context/appContext";
-import { fetchleaveTypes, updateLeaveTypeBasedOnOrg } from "@/app/_actions";
+import { fetchleaveTypes, insertNewLeaveType, updateLeaveTypeBasedOnOrg } from "@/app/_actions";
 import LeaveTypeDisable from "./leaveTypeDisable";
 
 interface LeaveType {
@@ -27,6 +33,7 @@ interface LeaveType {
 
 export default function Page() {
   const [leaveTypes, setLeaveTypes] = useState<LeaveType[]>();
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const { state: appState } = useApplicationContext();
   const { orgId } = appState;
@@ -47,15 +54,24 @@ export default function Page() {
     undefined
   );
   const [inActive, setInActive] = useState<undefined | LeaveType>(undefined);
-  const confirm: PopconfirmProps["onConfirm"] = async() => {
+  const confirm: PopconfirmProps["onConfirm"] = async () => {
     await updateLeaveTypeBasedOnOrg(true, orgId, inActive?.leaveTypeId);
   };
 
-
-
+  const [form] = Form.useForm();
+  const onFinish = async(values: any) => {
+     const newValues={...values,color:values.color.slice(1),orgId:orgId}
   
+    const data= await insertNewLeaveType(newValues)
+    if(data){
+      form.resetFields()
+      setModalVisible(false)
+    }
+    
+  };
+
   return (
-    <Row gutter={8}>
+    <Row gutter={24}>
       <Col span={24}>
         <Segmented
           value={segmentValue}
@@ -82,7 +98,6 @@ export default function Page() {
             else return !leaveType.isActive;
           })}
           renderItem={(item, index) => {
-            
             return (
               <List.Item
                 actions={[
@@ -149,6 +164,54 @@ export default function Page() {
             setDisableItem(undefined);
           }}
         />
+      </Col>
+      <Col>
+        <Button onClick={() => setModalVisible(true)} type="primary" style={{marginTop:'8px'}}>Add Leave Type</Button>
+        <Modal open={isModalVisible} 
+          footer={[
+            <Button key="cancel" onClick={() => setModalVisible(false)}>
+              Cancel
+            </Button>,
+            <Button key="submit" type="primary" onClick={() => form.submit()}>
+              Save
+            </Button>,
+          ]}
+          closable={false}>
+          <Form onFinish={onFinish} form={form}>
+            <Form.Item label="Leave Type name" name="name" rules={[{ required: true, message: 'Please Enter leave type  name' }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Leave Color" name="color" rules={[{ required: true, message: 'Please select leave type color' }]}>
+              <ColorPicker
+                onChange={(_, hex) => form.setFieldValue("color", hex)}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Set Slack Status"
+              name="setSlackStatus"
+              initialValue={false}
+            >
+              <Checkbox />
+            </Form.Item>
+            <Form.Item
+              label="Set Slack Status"
+              name="statusMsg"
+              initialValue="on Paid time of leave"
+            >
+              <Select>
+                <Select.Option value="on Paid time of leave">
+                  On Paid time off Leave
+                </Select.Option>
+                <Select.Option value="on Slack time of leave">
+                  On Slack Leave
+                </Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="Leave Type Emoji" name="emoji" initialValue="https://api.dicebear.com/7.x/miniavs/svg?seed=8">
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+            </Form.Item>
+          </Form>
+        </Modal>
       </Col>
     </Row>
   );
