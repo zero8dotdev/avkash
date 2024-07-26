ceaec317543a5904a442d926579ef882791ad604
gnani1226 <gnani@zero8.dev>
2024-07-26 13:27:27 +0530

    added accessing access token from supabase

--- src/app/_components/slack/handleBlockActions/handleBlockActions.ts
+++ src/app/_components/slack/handleBlockActions/handleBlockActions.ts
@@ -7,12 +7,20 @@ import reviewLeave from "../payload/reviewLeave";
 import handleYourLeaves from "../payload/handleYourLeaves";
 
 export default async function handleBlockActions(avkashUserInfo: avkashUserInfoProps, payload: any) {
-    const { view, trigger_id, actions ,callback_id} = payload;
+    const { view, trigger_id, actions } = payload;
     const triggerId = trigger_id;
     const viewId = view?.id;
     const userId = avkashUserInfo.userId;
     const actionId = actions?.[0]?.action_id;
     const selectedTeamId = actions?.[0]?.selected_option?.value;
+    const callbackId = view?.callback_id;
+    const values = actions?.[0]?.selected_option?.value;
+
+
+    if(callbackId && callbackId.startsWith('review_') && actionId === 'leave_type'){
+        const leaveId = callbackId.split('review_leave_')[1];
+        return reviewLeave(avkashUserInfo,actionId,leaveId,triggerId,callbackId,values,viewId);
+    }
 
     if (actionId.startsWith('review_')) {
         const leaveId = actionId.split('review_')[1];
@@ -26,15 +34,14 @@ export default async function handleBlockActions(avkashUserInfo: avkashUserInfoP
         return openRequestLeaveModal( avkashUserInfo, trigger_id,userId,true, payload );
  
     }
-
     switch (actionId) {
         case 'msg-req-leave':
         case 'home-req-leave':
             return openRequestLeaveModal(avkashUserInfo, triggerId, userId,false);
 
         case 'msg-your-leaves':
-            return handleYourLeaves();
-            // return new NextResponse('User leaves sent', { status: 200 });
+            const channelId = payload?.container?.channel_id;
+        return handleYourLeaves(avkashUserInfo,channelId);
 
         case 'msg-leave-reports':
             return new NextResponse('User leaves reports', { status: 200 });
@@ -49,7 +56,12 @@ export default async function handleBlockActions(avkashUserInfo: avkashUserInfoP
             return handleAppHomeOpened({ avkashUserInfo, yourDashboard: false });
 
         case 'your-dashboard':
+            console.log(actionId)
             return handleAppHomeOpened({ avkashUserInfo, yourDashboard: true });
+
+        case 'owner_select_team':
+            const ownerSelectedTeamId = view.state.values.owner_team_block.owner_select_team.selected_option.value; 
+            return handleAppHomeOpened({avkashUserInfo,yourDashboard: false,viewId,ownerSelectedTeamId})
 
         default:
             return new NextResponse('Unrecognized action', { status: 400 });
--- src/app/_components/slack/payload/handleYourLeaves.ts
+++ src/app/_components/slack/payload/handleYourLeaves.ts
@@ -1,7 +1,46 @@
+import { avkashUserInfoProps } from "@/app/api/slack/route";
 import { NextResponse } from "next/server";
+import { fetchLeavesHistory } from "../handleAppHomeOpened";
+import { sendPostMessages } from "../sendMessages";
+import { Divider } from "antd";
+import { calculateWorkingDays } from "../createCommonModalBlocks";
 
-export default function handleYourLeaves() {
-    console.log('hi from your leaves');
+export default async function handleYourLeaves(avkashUserInfo: avkashUserInfoProps, channelId: string) {
+    const userId = avkashUserInfo.userId;
+    const [allLeavesHistory, pendingHistory] = await fetchLeavesHistory({ days:30,userId });
+    const leavesHistory = await Promise.all(allLeavesHistory.map(async (leave) => {
+        const workingDays = await calculateWorkingDays(leave.orgId, leave.startDate, leave.endDate, leave.leaveType);
+        return {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": `${leave.leaveType} (${workingDays} working days) \n ${leave.startDate} - ${leave.endDate} - ${leave.isApproved} ${leave.isApproved === 'APPROVED' ? ':thumbsup:' : (leave.isApproved === 'PENDING' ? ':wink:' : ':laughing:')}`
+            // text: `\`${leave.leaveType}\` \`${leave.startDate}\` - \`${leave.endDate}\` (\`${workingDays}\` working days)`
+
+            }
+        };
+    }));
+
+
+    const blocks: any = [
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*Approve Leaves:*"
+            }
+        },
+        ...leavesHistory
+    ]
+
+
+    const responseText = 'Bleow are you Last 7 days leaves History';
+
+    sendPostMessages(channelId, responseText, blocks);
+
     return new NextResponse('User leaves sent', { status: 200 });
 
 }
