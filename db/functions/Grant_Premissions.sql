fb0bc6edb4b7b236626044e8b44acfa19c48f89b
Ashutosh Tripathi <hello@zero8.dev>
2024-07-27 17:37:38 +0530

    changes in onboarding

--- db/functions/Grant_Premissions.sql
+++ db/functions/Grant_Premissions.sql
@@ -1,3 +1,40 @@
+grant all privileges on all tables in schema public to postgres,
+anon,
+authenticated,
+service_role;
+
+grant all privileges on all functions in schema public to postgres,
+anon,
+authenticated,
+service_role;
+
+grant all privileges on all sequences in schema public to postgres,
+anon,
+authenticated,
+service_role;
+
+alter default privileges in schema public
+grant all on tables to postgres,
+anon,
+authenticated,
+service_role;
+
+alter default privileges in schema public
+grant all on functions to postgres,
+anon,
+authenticated,
+service_role;
+
+alter default privileges in schema public
+grant all on sequences to postgres,
+anon,
+authenticated,
+service_role;
+
+grant usage on schema "public" to anon;
+
+grant usage on schema "public" to authenticated;
+
 -- Grant usage on the public schema to supabase_auth_admin
 GRANT USAGE ON SCHEMA public TO supabase_auth_admin;
 
@@ -9,10 +46,25 @@ SELECT
   ON ALL TABLES IN SCHEMA public TO supabase_auth_admin;
 
 -- Grant execute on all functions in the public schema to supabase_auth_admin
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO supabase_auth_admin;
+GRANT
+EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO supabase_auth_admin;
 
-GRANT USAGE, SELECT ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
+GRANT USAGE,
+SELECT
+  ON SEQUENCE public."OrgActivityLog_id_seq" TO supabase_auth_admin;
+
 grant usage on schema "public" to anon;
+
 grant usage on schema "public" to authenticated;
-GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA "public" TO authenticated;
-GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA "public" TO anon;
+
+GRANT
+SELECT
+,
+  INSERT,
+UPDATE ON ALL TABLES IN SCHEMA "public" TO authenticated;
+
+GRANT
+SELECT
+,
+  INSERT,
+UPDATE ON ALL TABLES IN SCHEMA "public" TO anon;
--- src/app/_utils/supabase/adminClient.ts
+++ src/app/_utils/supabase/adminClient.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
-
-export default supabaseAdmin;
+export function createAdminClient() {
+  return createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+}
