d53dd72d4553a3f49e8b8b4e330809081dfd4889
yaswanthBonumaddi <yaswanthbonumaddi9@gmail.com>
2024-07-30 11:19:07 +0530

    fix: triggers related to  activity table

--- db/triggers/leave_policy_activity.sql
+++ db/triggers/leave_policy_activity.sql
@@ -9,42 +9,42 @@ BEGIN
     -- Check if the operation is an update
     IF TG_OP = 'UPDATE' THEN
         -- Compare each column and add to changedColumns if different
-        IF OLD.leaveTypeId IS DISTINCT FROM NEW.leaveTypeId THEN
-            changedColumns := jsonb_set(changedColumns, '{leaveTypeId}', jsonb_build_object('old', OLD.leaveTypeId, 'new', NEW.leaveTypeId));
+        IF OLD."leaveTypeId" IS DISTINCT FROM NEW."leaveTypeId" THEN
+            changedColumns := jsonb_set(changedColumns, '{leaveTypeId}', jsonb_build_object('old', OLD."leaveTypeId", 'new', NEW."leaveTypeId"));
         END IF;
-        IF OLD.unlimited IS DISTINCT FROM NEW.unlimited THEN
-            changedColumns := jsonb_set(changedColumns, '{unlimited}', jsonb_build_object('old', OLD.unlimited, 'new', NEW.unlimited));
+        IF OLD."unlimited" IS DISTINCT FROM NEW."unlimited" THEN
+            changedColumns := jsonb_set(changedColumns, '{unlimited}', jsonb_build_object('old', OLD."unlimited", 'new', NEW."unlimited"));
         END IF;
-        IF OLD.maxLeaves IS DISTINCT FROM NEW.maxLeaves THEN
-            changedColumns := jsonb_set(changedColumns, '{maxLeaves}', jsonb_build_object('old', OLD.maxLeaves, 'new', NEW.maxLeaves));
+        IF OLD."maxLeaves" IS DISTINCT FROM NEW."maxLeaves" THEN
+            changedColumns := jsonb_set(changedColumns, '{maxLeaves}', jsonb_build_object('old', OLD."maxLeaves", 'new', NEW."maxLeaves"));
         END IF;
-        IF OLD.accruals IS DISTINCT FROM NEW.accruals THEN
-            changedColumns := jsonb_set(changedColumns, '{accruals}', jsonb_build_object('old', OLD.accruals, 'new', NEW.accruals));
+        IF OLD."accruals" IS DISTINCT FROM NEW."accruals" THEN
+            changedColumns := jsonb_set(changedColumns, '{accruals}', jsonb_build_object('old', OLD."accruals", 'new', NEW."accruals"));
         END IF;
-        IF OLD.accrualFrequency IS DISTINCT FROM NEW.accrualFrequency THEN
-            changedColumns := jsonb_set(changedColumns, '{accrualFrequency}', jsonb_build_object('old', OLD.accrualFrequency, 'new', NEW.accrualFrequency));
+        IF OLD."accrualFrequency" IS DISTINCT FROM NEW."accrualFrequency" THEN
+            changedColumns := jsonb_set(changedColumns, '{accrualFrequency}', jsonb_build_object('old', OLD."accrualFrequency", 'new', NEW."accrualFrequency"));
         END IF;
-        IF OLD.accrueOn IS DISTINCT FROM NEW.accrueOn THEN
-            changedColumns := jsonb_set(changedColumns, '{accrueOn}', jsonb_build_object('old', OLD.accrueOn, 'new', NEW.accrueOn));
+        IF OLD."accrueOn" IS DISTINCT FROM NEW."accrueOn" THEN
+            changedColumns := jsonb_set(changedColumns, '{accrueOn}', jsonb_build_object('old', OLD."accrueOn", 'new', NEW."accrueOn"));
         END IF;
-        IF OLD.rollOver IS DISTINCT FROM NEW.rollOver THEN
-            changedColumns := jsonb_set(changedColumns, '{rollOver}', jsonb_build_object('old', OLD.rollOver, 'new', NEW.rollOver));
+        IF OLD."rollOver" IS DISTINCT FROM NEW."rollOver" THEN
+            changedColumns := jsonb_set(changedColumns, '{rollOver}', jsonb_build_object('old', OLD."rollOver", 'new', NEW."rollOver"));
         END IF;
-        IF OLD.rollOverLimit IS DISTINCT FROM NEW.rollOverLimit THEN
-            changedColumns := jsonb_set(changedColumns, '{rollOverLimit}', jsonb_build_object('old', OLD.rollOverLimit, 'new', NEW.rollOverLimit));
+        IF OLD."rollOverLimit" IS DISTINCT FROM NEW."rollOverLimit" THEN
+            changedColumns := jsonb_set(changedColumns, '{rollOverLimit}', jsonb_build_object('old', OLD."rollOverLimit", 'new', NEW."rollOverLimit"));
         END IF;
-        IF OLD.orgId IS DISTINCT FROM NEW.orgId THEN
-            changedColumns := jsonb_set(changedColumns, '{orgId}', jsonb_build_object('old', OLD.orgId, 'new', NEW.orgId));
+        IF OLD."orgId" IS DISTINCT FROM NEW."orgId" THEN
+            changedColumns := jsonb_set(changedColumns, '{orgId}', jsonb_build_object('old', OLD."orgId", 'new', NEW."orgId"));
         END IF;
-        IF OLD.updatedBy IS DISTINCT FROM NEW.updatedBy THEN
-            changedColumns := jsonb_set(changedColumns, '{updatedBy}', jsonb_build_object('old', OLD.updatedBy, 'new', NEW.updatedBy));
+        IF OLD."updatedBy" IS DISTINCT FROM NEW."updatedBy" THEN
+            changedColumns := jsonb_set(changedColumns, '{updatedBy}', jsonb_build_object('old', OLD."updatedBy", 'new', NEW."updatedBy"));
         END IF;
-        IF OLD.updatedOn IS DISTINCT FROM NEW.updatedOn THEN
-            changedColumns := jsonb_set(changedColumns, '{updatedOn}', jsonb_build_object('old', OLD.updatedOn, 'new', NEW.updatedOn));
+        IF OLD."updatedOn" IS DISTINCT FROM NEW."updatedOn" THEN
+            changedColumns := jsonb_set(changedColumns, '{updatedOn}', jsonb_build_object('old', OLD."updatedOn", 'new', NEW."updatedOn"));
         END IF;
 
         -- Insert the log entry only if there are changes
-        IF jsonb_array_length(changedColumns) > 0 THEN
+        IF changedColumns <> '{}'::jsonb THEN
             INSERT INTO public."ActivityLog" ("tableName", "orgId", "changedColumns", "changedBy", keyword)
             VALUES (tableName, NEW."orgId", changedColumns, NEW."updatedBy", 'change');
         END IF;
