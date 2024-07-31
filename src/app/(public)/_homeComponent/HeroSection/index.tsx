379dbf3d243e2b61898ccabdc578d81b17d7f785
Ashutosh Tripathi <hello@zero8.dev>
2024-07-31 16:25:47 +0530

    content flow first draft on web

--- src/app/(public)/_homeComponent/HeroSection/index.tsx
+++ src/app/(public)/_homeComponent/HeroSection/index.tsx
@@ -5,22 +5,23 @@ const HeroSection = () => {
   return (
     <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32 h-screen">
       <h1 className="mx-auto max-w-4xl text-5xl font-semibold tracking-tight text-slate-900 md:text-7xl font-firaSans ">
-        Leave{" "}
+        Specify.&nbsp;
         <span className="relative whitespace-nowrap text-blue-600">
-          <span className="relative">made simple</span>
-        </span>{" "}
-        for small businesses.
+          <span className="relative">Automate.&nbsp;</span>
+        </span>
+        Elevate&nbsp;
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        Most HRMS software is accurate, but hard to use. We make the opposite
-        trade-off, and hope you don’t get audited.
+        <span className="text-blue-600">Avkash,&nbsp;</span>a leave management
+        HR automation tool, integrates seamlessly with Slack and Google
+        Workspace to streamline your leave management process.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Link
           href="#"
           className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
         >
-          Let&apos;s Get Started
+          Add to Slack
         </Link>
         <Link
           href="#"
@@ -38,4 +39,5 @@ const HeroSection = () => {
     </div>
   );
 };
+
 export default HeroSection;
--- src/app/(public)/_homeComponent/faq/index.tsx
+++ src/app/(public)/_homeComponent/faq/index.tsx
@@ -1,62 +1,67 @@
-'use client';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
 
 const faqItems = [
-    {
-        question: 'How to Request Leave with Avkash?',
-        answer: 'Go the the Avkash bot click on the request leave button add the form details and hit submit your leave will be saved and sent it to the manager.',
-    },
-    {
-        question: 'How to Contact Support?',
-        answer: 'To contact support, send an email to support@avkash.io. Alternatively, fill out the contact form here.',
-    },{
-        question: 'Configuring Avkash - A Quick Start Guide?',
-        answer: 'select a organization first and add your team member in it, add a necessary data In it.if you have any doubt contact us at support@zero8.dev ',
-    },{
-        question: 'What is Avkash ?',
-        answer: 'Avkash is a slack app which manages the leaves and track leaves..',
-    },{
-        question: 'What Are The Different User Roles In Avkash?',
-        answer: 'There are primary 3 roles in the avkash Owner, Manager and User',
-    },{
-        question: 'How to Send Notifications For Leave Changes Plus Daily and Weekly Summaries?',
-        answer: 'After change in any leave in the Avkash the leave notification will get to users.',
-    },
-    {
-        question: 'How To Add And Edit A Leave Policy Or Type.?',
-        answer: 'Only owner and manager can edit the Leave Policy and Type. In the settings leave setting tab you can change the leave policy and leave type. ',
-    },
+  {
+    question: "Why should I use an automated leave management tool?",
+    answer:
+      "An automated leave management tool ensures accurate record-keeping, and enhances team coordination without extra hassle of manual tracking. It promotes transparency, boosts productivity, and empowers employees with self-service options.",
+  },
+  {
+    question: "Who is Avkash for?",
+    answer:
+      "An automated leave management tool ensures accurate record-keeping, and enhances team coordination without extra hassle of manual tracking. It promotes transparency, boosts productivity, and empowers employees with self-service options.",
+  },
+  {
+    question: "How does Avkash handle leave approvals?",
+    answer:
+      "Leave requests submitted through Avkash are routed to the designated approvers based on the configured approval workflow. Approvers can review, approve, or deny leave requests directly within Slack or Google Workspace, and real-time notifications keep everyone updated.",
+  },
+  {
+    question: "How secure is my data with Avkash?",
+    answer:
+      "**Data security is a top priority for Avkash. We implement advanced security measures to protect your data. Please refer to our data and privacy policy for more details.**",
+  },
+  {
+    question: "Can I try Avkash before committing to a subscription?",
+    answer:
+      "Yes, we offer a free trial so you can experience Avkash and see how it benefits your team. Sign up for 14 days free trial and start exploring Avkash’s features without any commitment.",
+  },
 ];
 
 const Faq: React.FC = () => {
-    return (
-        <div id='faq' className="w-full flex flex-col items-center my-24 ">
-            <section className="bg-slate-50 py-20 sm:py-32">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
-                        <h2 className="text-3xl tracking-tight text-slate-900 sm:text-4xl">
-                            Frequently asked questions
-                        </h2>
-                        <p className="mt-4 text-lg tracking-tight text-slate-700">
-                            If you can’t find what you’re looking for, email our support team and if you’re lucky someone will get back to you.
-                        </p>
-                    </div>
-                    <ul className=" mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3">
-                        {faqItems.map((item, index) => (
-                            <li key={index}>
-                                <ul className="flex flex-col gap-y-8">
-                                    <li>
-                                        <h3 className=" text-lg leading-7 text-slate-900">{item.question}</h3>
-                                        <p className="mt-4 text-sm text-slate-700">{item.answer}</p>
-                                    </li>
-                                </ul>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </section>
+  return (
+    <div id="faq" className="w-full flex flex-col items-center my-24">
+      <section className="bg-slate-50 py-12">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl tracking-tight text-slate-900 sm:text-4xl">
+              Frequently asked questions
+            </h2>
+          </div>
+          <ul className=" mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3">
+            {faqItems.map((item, index) => (
+              <li key={index}>
+                <ul className="flex flex-col gap-y-8">
+                  <li>
+                    <h3 className=" text-lg leading-7 text-slate-900">
+                      {item.question}
+                    </h3>
+                    <p className="mt-4 text-sm text-slate-700">{item.answer}</p>
+                  </li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+          <p className="mt-12 text-lg tracking-tight text-slate-700">
+            If you can&apos;t find what you&apos;re looking for, Read more{" "}
+            <Link href="/faq">here</Link>
+          </p>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Faq;
