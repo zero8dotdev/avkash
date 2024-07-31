d031e0a0bbbece136204bd65a42e6cc0a721522a
Ashutosh Tripathi <hello@zero8.dev>
2024-07-31 13:40:46 +0530

    fixed build errors

--- src/app/(public)/_homeComponent/contactUs/index.tsx
+++ src/app/(public)/_homeComponent/contactUs/index.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
-interface contactProps {
+"use client";
+
+import { useState } from "react";
+interface ContactProps {
   isOpen: boolean;
   closeModal: any;
 }
 
-const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
-
-  const [formData, setFormData] = useState({ email: '', subject: '', message: '' });
-  const [responseMessage, setResponseMessage] = useState('');
+const ContactModal: React.FC<ContactProps> = ({ isOpen, closeModal }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [responseMessage, setResponseMessage] = useState("");
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
@@ -18,21 +23,21 @@ const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
     e.preventDefault();
 
     try {
-      const res = await fetch('/api/contact', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
 
       const result = await res.json();
       if (result.success) {
-        setResponseMessage('Message sent successfully!');
+        setResponseMessage("Message sent successfully!");
       } else {
         setResponseMessage(`Failed to send message: ${result.error}`);
       }
     } catch (error: any) {
       setResponseMessage(`An error occurred: ${error.message}`);
-    } 
+    }
     closeModal();
   };
 
@@ -42,14 +47,17 @@ const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
           <div className=" rounded-lg shadow-lg bg-gray-800 w-96">
             <div className="flex justify-between items-center p-5 border-b border-gray-700">
-              <h5 className="text-xl font-medium text-white">
-                Contact Avkash
-              </h5>
+              <h5 className="text-xl font-medium text-white">Contact Avkash</h5>
               <button
                 onClick={closeModal}
                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
               >
-                <svg className="w-4 h-4" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
                   <path
                     stroke="currentColor"
                     strokeLinecap="round"
@@ -62,7 +70,10 @@ const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
             </div>
             <form className="p-5" onSubmit={handleSubmit}>
               <div className="mb-4">
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-white"
+                >
                   Your email
                 </label>
                 <input
@@ -77,7 +88,10 @@ const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
                 />
               </div>
               <div className="mb-4">
-                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label
+                  htmlFor="subject"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
                   Subject
                 </label>
                 <input
@@ -92,12 +106,15 @@ const ContactModal: React.FC<contactProps> = ({ isOpen, closeModal }) => {
                 />
               </div>
               <div className="mb-4">
-                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
                   Your message
                 </label>
                 <textarea
                   id="message"
-                  name='message'
+                  name="message"
                   value={formData.message}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Your message..."
