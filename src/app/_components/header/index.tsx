8c4be06df007ec95f24efa6ef7b622232a282a43
rohitKoktare <rohitkoktarebugatti@gmai.com@example.com>
2024-08-02 15:35:26 +0530

    header update

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -8,35 +8,23 @@ import MainMenu from "./_components/main-menu";
 import logo from '../../(public)/avkash-only.png'
 export default function AppHeader() {
   return (
+
       <Header
         style={{
           display: "flex",
-          justifyContent: "flex-start",
+          justifyContent: "space-between",
           alignItems: "center",
           backgroundColor: "#fff",
         }}
-        className=""
       >
-        <Link href="/" style={{flex:1}}>
-          <Title level={3} style={{ marginTop: "0px", marginBottom: "0px" }}>
-            avkash
-          </Title>
-        </Link>
+      <Link href="/" className="flex justify-center align-center"> 
+        <Image src={logo} alt="avkash" height={50} width={50}/>
+        <Title className="hidden sm:block m-0 mt-2 leading-none" style={{marginBottom:'0px',fontWeight:300, lineHeight:'30px', fontSize:30}} >avkash</Title>
+      </Link>
         <MainMenu />
-        {/* <div className="flex flex-1 items-center mx-10 font-semibold" >
-          <ul className="flex gap-x-10">
-            <li>
-              <Link href="/#faq" className="scroll-smooth">FAQ</Link></li>
-            <li><Link href='/#priceSection' className="tracking-wider">Pricing</Link></li>
-          </ul>
-        </div> */}
         <Space >
-          {/* <Link href="/login">
-            <Button type="primary">Sign up</Button>
-          </Link> */}
           <LogoutButton />
         </Space>
       </Header>
-
   );
 }
