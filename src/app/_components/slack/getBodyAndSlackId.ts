699b97d4c164680b3e18d11f13af8a930fa9b8b3
gnani1226 <gnani@zero8.dev>
2024-08-08 13:41:16 +0530

    modified code to get the end point verified

--- src/app/_components/slack/getBodyAndSlackId.ts
+++ src/app/_components/slack/getBodyAndSlackId.ts
@@ -10,20 +10,20 @@ export default async function getBodyAndSlackId(request: NextRequest): Promise<a
   let body: BodyProps = {};
   let currentUserSlackId: string = '';
   try {
-    if (contentType === 'application/x-www-form-urlencoded') {
+    if (contentType === 'application/json') {
+      body = await request.json();
+      // if (body.type === 'url_verification') {
+      //   return new NextResponse(JSON.stringify({ challenge: body.challenge }), {
+      //     status: 200,
+      //     headers: { 'Content-Type': 'application/json' }
+      //   });
+      // }
+      currentUserSlackId = body.event.user;
+    }else if (contentType === 'application/x-www-form-urlencoded') {
       const params = new URLSearchParams(await request.text());
       body = Object.fromEntries(params.entries());
       currentUserSlackId = JSON.parse(body.payload).user.id;
 
-    } else if (contentType === 'application/json') {
-      body = await request.json();
-      if (body.type === 'url_verification') {
-        return new NextResponse(JSON.stringify({ challenge: body.challenge }), {
-          status: 200,
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }
-      currentUserSlackId = body.event.user;
     }
     return [body, currentUserSlackId];
   } catch (error) {
