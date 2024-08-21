de8e32344c4a4441ece07e1df41c91166fd6aba9
gnani1226 <gnani@zero8.dev>
2024-08-21 17:48:48 +0530

    added loading view when opening any modal not to fail the trigger_expired_error and also made changes to send review request only to the manager

--- src/app/_components/slack/payload/openRequestLeaveModal.ts
+++ src/app/_components/slack/payload/openRequestLeaveModal.ts
@@ -1,45 +1,10 @@
-// import { avkashUserInfoProps } from "@/app/api/slack/route";
-// import { createCommonModalBlocks } from "../createCommonModalBlocks";
-// import { NextResponse } from "next/server";
-// import { openView, updateViews } from "../sendMessages";
-
-// export async function openRequestLeaveModal(avkashUserInfo: avkashUserInfoProps, trigger_id: string, user_id: string, checkLeaveType: boolean, payload?: any) {
-//     let viewId: string = '';
-//     if (payload) {
-//         const { view } = payload;
-//         viewId = view?.id;
-
-//     }
-//     const commonBlocks = await createCommonModalBlocks({ avkashUserInfo, checkLeaveType, payload });
-//     const view = {
-//         type: 'modal',
-//         callback_id: 'home-req-leave',
-//         title: { type: 'plain_text', text: 'Request Leave' },
-//         submit: { type: 'plain_text', text: 'Submit', emoji: true },
-//         blocks: commonBlocks
-//     }
-
-//     if (checkLeaveType) {
-//         updateViews(avkashUserInfo,viewId, view)
-//     } else {
-//         openView(avkashUserInfo,
-//             trigger_id,
-//             view
-//         );
-//     }
-
-//     return new NextResponse('Modal opened', { status: 200 });
-// }
-
-
-
 import { avkashUserInfoProps } from "@/app/api/slack/route";
 import { createCommonModalBlocks } from "../createCommonModalBlocks";
 import { NextResponse } from "next/server";
 import { openView, updateViews } from "../sendMessages";
 
 export async function openRequestLeaveModal(avkashUserInfo: avkashUserInfoProps, trigger_id: string, user_id: string, checkLeaveType: boolean, payload?: any) {
-    let viewId: string = '';
+    let viewId: string = payload && payload.view.id;
 
     const loadingView = {
         type: 'modal',
@@ -52,11 +17,7 @@ export async function openRequestLeaveModal(avkashUserInfo: avkashUserInfoProps,
             }
         ]
     };
-
-
     const view_id = await openView(avkashUserInfo, trigger_id, loadingView);
-
-
     const commonBlocks = await createCommonModalBlocks({ avkashUserInfo, checkLeaveType, payload });
 
     const fullView = {
@@ -68,7 +29,7 @@ export async function openRequestLeaveModal(avkashUserInfo: avkashUserInfoProps,
     };
 
     if (checkLeaveType) {
-        await updateViews(avkashUserInfo, viewId || view_id, fullView);
+        await updateViews(avkashUserInfo, viewId, fullView);
 
     } else {
         await updateViews(avkashUserInfo, view_id, fullView);
