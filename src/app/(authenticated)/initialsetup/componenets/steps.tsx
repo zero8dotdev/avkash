1650c7d919fefae02a55c13b52f235be36af632a
yaswanthBonumaddi <yaswanthbonumaddi9@gmail.com>
2024-12-14 13:02:53 +0530

    fix: setup

--- src/app/(authenticated)/initialsetup/componenets/steps.tsx
+++ src/app/(authenticated)/initialsetup/componenets/steps.tsx
@@ -1,6 +1,17 @@
-import { CalendarFilled, EnvironmentFilled, NotificationFilled, NotificationOutlined, SettingFilled, SettingOutlined, SlackCircleFilled, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
-import { Col, Steps } from 'antd';
-import React from 'react';
+import {
+  CalendarFilled,
+  EnvironmentFilled,
+  NotificationFilled,
+  NotificationOutlined,
+  SettingFilled,
+  SettingOutlined,
+  SlackCircleFilled,
+  SmileOutlined,
+  SolutionOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Col, Steps } from "antd";
+import React from "react";
 
 interface TopStepsProps {
   position: number;
@@ -8,37 +19,35 @@ interface TopStepsProps {
 
 const TopSteps: React.FC<TopStepsProps> = ({ position }) => {
   return (
-    <Col span={22} push={2}>
-      <Steps
-        current={position}
-        items={[
-          {
-            title: "Connect to Slack",
-            icon: <SlackCircleFilled />,
-          },
-          {
-            title: "Settings",
-            icon:<SettingFilled />,
-          },
-          {
-            title: "Leave Policy",
-            icon: <CalendarFilled />,
-          },
-          {
-            title: "Locations",
-            icon: <EnvironmentFilled />,
-          },
-          {
-            title: "Notifications",
-            icon: <NotificationFilled />,
-          },
-          {
-            title: "Invite Users",
-            icon: <UserOutlined />,
-          },
-        ]}
-      />
-    </Col>
+    <Steps
+      current={position}
+      items={[
+        {
+          title: "Connect to Slack",
+          icon: <SlackCircleFilled />,
+        },
+        {
+          title: "Settings",
+          icon: <SettingFilled />,
+        },
+        {
+          title: "Leave Policy",
+          icon: <CalendarFilled />,
+        },
+        {
+          title: "Locations",
+          icon: <EnvironmentFilled />,
+        },
+        {
+          title: "Notifications",
+          icon: <NotificationFilled />,
+        },
+        {
+          title: "Invite Users",
+          icon: <UserOutlined />,
+        },
+      ]}
+    />
   );
 };
 
