ba7107be40f486554d5a4a1d813e3fafc2d42e5b
kesavaDk <kesavad1652@gmail.com>
2024-12-22 13:35:14 +0530

    feat: loading

--- src/app/(authenticated)/initialsetup/connect-slack/page.tsx
+++ src/app/(authenticated)/initialsetup/connect-slack/page.tsx
@@ -8,7 +8,7 @@ import {
 } from "@ant-design/icons";
 import { Flex, Button, Row, Col, Steps, Card } from "antd";
 import { useRouter } from "next/navigation";
-import TopSteps from "../componenets/steps";
+import TopSteps from "../_componenets/steps";
 
 export default function AddToSlack() {
   const router = useRouter();
@@ -42,17 +42,17 @@ export default function AddToSlack() {
             alignItems: "center", // Vertical alignment
           }}
         >
-            <a
-              href={`https://slack.com/oauth/v2/authorize?client_id=6356258938273.7279987270326&scope=app_mentions:read,channels:history,channels:read,chat:write,chat:write.public,commands,groups:read,im:history,im:read,mpim:history,users:read,users:read.email&user_scope=channels:history,channels:read,groups:read,im:history,im:read,mpim:history,mpim:read&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}welcome/install-to-slack`}
-            >
-              <img
-                alt="Add to Slack"
-                height="40"
-                width="139"
-                src="https://platform.slack-edge.com/img/add_to_slack.png"
-                srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-              />
-            </a>
+          <a
+            href={`https://slack.com/oauth/v2/authorize?client_id=6356258938273.7279987270326&scope=app_mentions:read,channels:history,channels:read,chat:write,chat:write.public,commands,groups:read,im:history,im:read,mpim:history,users:read,users:read.email&user_scope=channels:history,channels:read,groups:read,im:history,im:read,mpim:history,mpim:read&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}welcome/install-to-slack`}
+          >
+            <img
+              alt="Add to Slack"
+              height="40"
+              width="139"
+              src="https://platform.slack-edge.com/img/add_to_slack.png"
+              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            />
+          </a>
         </Card>
         <Flex justify="end">
           {/* <Button danger icon={<LeftOutlined />}>
