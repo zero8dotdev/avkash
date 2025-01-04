e12a89f94a8aee25826c5f528eb4a888090b3b0e
yaswanthBonumaddi <yaswanthbonumaddi9@gmail.com>
2025-01-04 23:52:48 +0530

    fix: teamsettings-completed

--- src/app/(authenticated)/dashboard/teams/[teamId]/notifications/page.tsx
+++ src/app/(authenticated)/dashboard/teams/[teamId]/notifications/page.tsx
@@ -1,10 +1,37 @@
 "use client";
-import { Card, Col, Form, Row } from "antd";
+import { Button, Card, Col, Form, Row } from "antd";
 import React from "react";
 import TeamSettingsTabs from "../_components/team-settings-tabs";
-import Notification from "@/app/_components/notification";
+import Notification from "@/app/(authenticated)/dashboard/teams/[teamId]/_components/notification";
+import { useParams } from "next/navigation";
+import useSWR from "swr";
+import { fetchTeamGeneralData, updateTeamNotifications } from "../_actions";
 
 const Page = () => {
+
+  const { teamId } = useParams() as { teamId: string };
+  // Fetch team data
+  const fetcher = async (key: string) => {
+    const team = key.split("*")[1];
+    return await fetchTeamGeneralData(team);
+  };
+
+  const {
+    data: team,
+    error,
+    mutate,
+    isValidating: teamLoading,
+  } = useSWR(`teamNotifications*${teamId}`, fetcher);
+
+  const handleFormSubmit = async (values: any) => {
+    try {
+      await updateTeamNotifications(teamId, values);
+      mutate({...team, ...values}, false);
+    } catch (err) {
+      console.error("Failed to update notifications:", err);
+    }
+  };
+
   return (
     <Row>
       <Col span={3}>
@@ -12,8 +39,13 @@ const Page = () => {
       </Col>
       <Col span={16}>
         <Card title="Team Notifications">
-          <Form onFinish={(values) => console.log(values)}>
+          <Form   initialValues={team} onFinish={handleFormSubmit}>
             <Notification />
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Save
+              </Button>
+            </Form.Item>
           </Form>
         </Card>
       </Col>
--- src/app/(authenticated)/dashboard/teams/[teamId]/users/page.tsx
+++ src/app/(authenticated)/dashboard/teams/[teamId]/users/page.tsx
@@ -1,15 +1,60 @@
-import { Card, Col, Row } from "antd";
+"use client";
+
+import { Avatar, Button, Card, Col, List, Row } from "antd";
 import React from "react";
 import TeamSettingsTabs from "../_components/team-settings-tabs";
+import { useParams } from "next/navigation";
+import { fetchTeamUsersData } from "../_actions";
+import useSWR from "swr";
+import { clear } from "console";
 
 const Page = () => {
+  const { teamId } = useParams() as { teamId: string };
+  // Fetch team data
+  const fetcher = async (key: string) => {
+    const team = key.split("*")[1];
+    return await fetchTeamUsersData(team);
+  };
+
+  const {
+    data: users,
+    error,
+    mutate,
+    isValidating: managersLoading,
+  } = useSWR(`teamUsers*${teamId}`, fetcher);
+
+  console.log(users);
+
   return (
     <Row>
       <Col span={3}>
         <TeamSettingsTabs position="users" />
       </Col>
       <Col span={16}>
-        <Card title="Team Users"></Card>
+        <Card title="Team Users">
+          <List
+            bordered
+            dataSource={users}
+            renderItem={(item) => (
+              <List.Item style={{ cursor: "pointer" }}>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar style={{ backgroundColor: "#227b83" }}>
+                      {" "}
+                      {item.name
+                        .split(" ")
+                        .map((word: any) => word[0])
+                        .join("")
+                        .toUpperCase()}{" "}
+                    </Avatar>
+                  }
+                  title={item?.name}
+                  description={item?.email}
+                />
+              </List.Item>
+            )}
+          />
+        </Card>
       </Col>
     </Row>
   );
