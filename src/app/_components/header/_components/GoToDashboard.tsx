3490bd510c2c506c4089e085e73adc6489d94661
Naman Tripathi <namantripathi937@gmail.com>
2025-04-26 18:27:07 +0530

    feat: go to dashboard button loader animation

--- src/app/_components/header/_components/GoToDashboard.tsx
+++ src/app/_components/header/_components/GoToDashboard.tsx
@@ -2,21 +2,35 @@
 
 import { useApplicationContext } from '@/app/_context/appContext';
 import { usePathname, useRouter } from 'next/navigation';
-import router from 'next/router';
+import { useState } from 'react';
 
 export default function GoToDashboard() {
+  const [isLoading, setIsLoading] = useState(false);
   const { state } = useApplicationContext();
   const { user } = state;
   const pathname = usePathname();
+  const router = useRouter();
 
-  if (pathname !== '/' || !user) return null;
+  const shouldShowButton =
+    (pathname === '/' && user) || pathname?.startsWith('/initialsetup'); // 'Go to Dashboard' button should be visible only on the initial setup pages or front page if user is already logged in
+
+  if (!shouldShowButton) return null;
+
+  const navigationHandler = () => {
+    setIsLoading(true);
+    router.push('/dashboard/timeline');
+  };
 
   return (
     <button
-      onClick={() => router.push('/dashboard/timeline')}
+      onClick={navigationHandler}
       className="ml-auto mr-4 inline-flex align-items-end rounded-full py-2 px-4 text-sm  focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
     >
-      Go to Dashboard
+      {isLoading ? (
+        <span className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-white border-r-transparent" />
+      ) : (
+        'Go to Dashboard'
+      )}
     </button>
   );
 }
