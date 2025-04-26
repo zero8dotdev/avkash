f99d34946f2b29bb74469b7190b310fcbc1901dc
Naman Tripathi <namantripathi937@gmail.com>
2025-04-26 16:19:41 +0530

    fix: centered add to slack and sign in button

--- src/app/(public)/add-to-slack/page.tsx
+++ src/app/(public)/add-to-slack/page.tsx
@@ -16,14 +16,12 @@ export default function AddToSlack() {
   };
 
   return (
-    <Card
+    <div
       style={{
-        margin: '25px 0px 25px 0px',
-        minHeight: '300px',
-        overflow: 'auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '85vh',
       }}
     >
       <a
@@ -31,12 +29,12 @@ export default function AddToSlack() {
       >
         <img
           alt="Add to Slack"
-          height="40"
-          width="139"
+          height="70"
+          width="160"
           src="https://platform.slack-edge.com/img/add_to_slack.png"
           srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
         />
       </a>
-    </Card>
+    </div>
   );
 }
