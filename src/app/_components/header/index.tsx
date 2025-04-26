f0871e60e7968ffcd132220446e46d7919b6dbcc
Naman Tripathi <namantripathi937@gmail.com>
2025-04-26 17:18:49 +0530

    feat: go to dashboard button on the front page

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,13 +1,18 @@
+'use client';
+
 import Link from 'next/link';
 import { Space } from 'antd';
 import Image from 'next/image';
 import Title from 'antd/es/typography/Title';
 import { Header } from 'antd/es/layout/layout';
+import { usePathname, useRouter } from 'next/navigation';
 import LogoutButton from './_components/logout';
 import MainMenu from './_components/main-menu';
 import logo from '../../(public)/avkash-logo-new.png';
 
 export default function AppHeader() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <Header
       style={{
@@ -40,6 +45,14 @@ export default function AppHeader() {
         </Title>
       </Link>
       <MainMenu />
+      {pathname === '/' && (
+        <button
+          onClick={() => router.push('/dashboard/timeline')}
+          className="ml-auto mr-4 inline-flex align-items-end rounded-full py-2 px-4 text-sm  focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
+        >
+          Go to Dashboard
+        </button>
+      )}
       <Space>
         <LogoutButton />
       </Space>
